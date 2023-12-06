f89fa9a0e4d064ae70135d36c233bfd334c3c816
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-07 01:16:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "John", 15);
+            Student student2 = new Student(40, "Tim", 10);
+
+            Console.WriteLine(student1.IsObese());
+            Console.WriteLine(student2.IsObese());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;
+            this.name = aName;
+            this.age = aAge;
+        }
+        
+        public bool IsObese()
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
